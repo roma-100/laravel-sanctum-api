ed268e685d2afb88723ec2dd783f1ed7e562b93a
roma-100 <roma-100@mail.ru>
2023-01-04 22:30:15 +0600

    auth Step01

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 //use App\Models\Operation;
 use App\Http\Controllers\OperationController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,23 +15,29 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::resource('operations', OperationController::class);
-Route::post('/operations', [OperationController::class, 'store']);
+// Public routes
+//Route::resource('operations', OperationController::class);
+Route::post('/register', [AuthController::class, 'register']);
+Route::get('/operations', [OperationController::class, 'index']);
+Route::get('/operations/{id}', [OperationController::class, 'show']);
 Route::get('/operations/search/{type}', [OperationController::class, 'search']);
+
 /* Route::get('/operations', [OperationController::class, 'index']);
 Route::get('/operations/{id}', [OperationController::class, 'show']);
 Route::post('/operations', [OperationController::class, 'store']); */
 
-/* Route::post('/operations', function(){
-    return Operation::create([ 
-        'type' => 'type One',
-        'name' => 'name-one',
-        'slug' => 'slug-one',
-        'description' => 'description-one',
-        'usd' => '99.99',
-    ]);
+// Protected routes
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    //
+    Route::post('/operations', [OperationController::class, 'store']);
+    Route::put('/operations/{id}', [OperationController::class, 'update']);
+    Route::delete('/operations/{id}', [OperationController::class, 'destroy']);
 });
- */
+
+/* Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+}); */
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
